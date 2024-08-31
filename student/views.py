cccd7b021b43394052f9680396ba834083356911
pruthimayank <pruthimayank2003@gmail.com>
2024-09-01 01:29:17 +0530

    initial setup

---
+++ student/views.py
@@ -0,0 +1,7 @@
+import requests
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def login(requests):
+    return HttpResponse("Any student of HTML Here")
