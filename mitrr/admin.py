42df56bdfdbdcdee2b5a2a90fae6601861c1eaa2
pruthimayank <pruthimayank2003@gmail.com>
2024-09-01 03:12:19 +0530

    session added

--- mitrr/admin.py
+++ mitrr/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, StudentList, TeacherList
+from .models import User, CustomSession,StudentList, TeacherList
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(CustomSession)
 admin.site.register(StudentList)
 admin.site.register(TeacherList)
--- mitrr/models.py
+++ mitrr/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class User(models.Model):
@@ -8,6 +9,16 @@ class User(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+class CustomSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    usertype = models.CharField(max_length=100)
+    userdata = models.JSONField()
+    jwttoken = models.UUIDField(default=uuid.uuid4, unique=True)
+    created_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"Session for {self.user.username}"
 
 class StudentList(models.Model):
     enroll_number = models.CharField(max_length=100)
