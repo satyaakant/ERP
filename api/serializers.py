fbbc50c0aebdd6043e8403412233f085e969c50d
satyaaa <satyakantmishra50@gmail.com>
2024-09-11 23:01:56 +0530

    year removed

--- api/serializers.py
+++ api/serializers.py
@@ -95,4 +95,4 @@ class AttendanceReportSerializer(serializers.ModelSerializer):
 class BatchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Batch
-        fields = ['batch_name', 'year', 'description', 'semester']
+        fields = ['batch_name', 'description', 'semester']
